f55b0effe571fd5229b5eb74d1e504e319a58e79
SebastianOso <sebastian.oso03@gmail.com>
2025-02-13 10:37:15 -0600

    funciones de consola en js

--- lab3_enclase/js/introjs.js
+++ lab3_enclase/js/introjs.js
@@ -1,1 +1,11 @@
 // consola (log, info, warn, error, assert)
+console.log("hola mundo wookie");
+console.info("Aqui andamos aprendiendo");
+console.warn("Esto es una advertencia");
+console.error("dsajfndjskancjkdankcsanl");
+console.assert(1 == 1);
+console.assert(1 === 1);
+console.assert(1 == "1");
+console.assert(1 === "1");
+console.assert(1 == true);
+console.assert(1 === true);
