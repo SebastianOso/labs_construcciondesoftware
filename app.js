6fc512b616603ee86012358efa7fa4e8a08f0239
SebastianOso <sebastian.oso03@gmail.com>
2025-02-25 10:15:18 -0600

    agregar formato

--- app.js
+++ app.js
@@ -5,14 +5,24 @@ const app = express();// se inicializa la estancia de express y se inicia el ser
 //si queremos agregar algo a la aplicacion se agrega un middleware
 //registas el middleware y todo eso se registra en toda la aplicacion
 //para declarar un middleware es la sig nomenclatura
+
 app.use((request, response, next) => { //next es una funcion de express que lo manda a la siguiente cosa
-    console.log('Middleware!');//que salga middleware en la consola
-    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+    //request tiene toda la informacion de la peticion 
+    //response tiene toda la informaion de la respuesta que quieres mandar
+    //que salga middleware en la consola
+    console.log('Middleware!');
+    
+    //Le permite a la petición avanzar hacia el siguiente middleware
+    next(); 
+    
 });
 
+//es un composite
 app.use((request, response, next) => {
-    console.log('Otro middleware!');//es un composite
-    response.send('¡Hola mundo!'); //Manda la respuesta
+    console.log('Otro middleware!');
+    
+     //Manda la respuesta
+    response.send('¡Hola mundo!');
 });
 
 app.listen(3000);
