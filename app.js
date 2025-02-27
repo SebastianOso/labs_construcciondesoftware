6b12e4b443846548eb0935edfd3f4c1a4ac48b73
SebastianOso <sebastian.oso03@gmail.com>
2025-02-27 09:57:01 -0600

    añadir path a app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'); //para usar express, se necesita un requiere, se guarda en una constante
 const app = express();// se inicializa la estancia de express y se inicia el servidor
 
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 const bodyParser = require('body-parser');
