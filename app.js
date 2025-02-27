00e2265004140227cf078e90a06b3122434406b9
SebastianOso <sebastian.oso03@gmail.com>
2025-02-27 10:14:09 -0600

    configurar EJS

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();// se inicializa la estancia de express y se inicia el ser
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));//para que todas mis rutas se use body parser
 
