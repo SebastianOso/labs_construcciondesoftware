50ddcb91747e775d003efc0a092a41195f5c23c8
SebastianOso <sebastian.oso03@gmail.com>
2025-03-01 14:08:00 -0600

    lab 8 totalmente done

--- old_labs/lab8/js/app.js
+++ old_labs/lab8/js/app.js
@@ -16,6 +16,20 @@ const crearArchTexto = (mensaje) => {
     file_system.writeFileSync('hola.txt', mensaje)
 }
 
+const fizzbuzz = (n) => {
+    for (i=0; i <= n;i++) {
+        if (i % 15 === 0){
+            console.log("FizzBuzz")
+        } else if (i % 3 === 0 ){
+            console.log("Fizz")
+        } else if (i % 5 === 0){
+            console.log("Buzz")
+        }
+    }
+}
+
+fizzbuzz(100);
+
 crearArchTexto('hola como estas')
 
 const html = `
