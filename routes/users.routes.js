31737ede2a66c47a23d2655c921d89b8fce66a83
SebastianOso <sebastian.oso03@gmail.com>
2025-03-04 09:39:47 -0600

    hacer commit en rama lab13

---
+++ routes/users.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const users_controller = requiere('../controllers/users.controller')
+
+router.get('/login', users_controller.get_login)
+router.post('/login', users_controller.post_login)
+
+module.exports = router;
