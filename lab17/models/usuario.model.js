700270eb3114804786f820cebdefd59341a78bf1
SebastianOso <sebastian.oso03@gmail.com>
2025-03-10 10:16:02 -0600

    cifrar contraseña con bcryptjs

--- lab17/models/usuario.model.js
+++ lab17/models/usuario.model.js
@@ -1,4 +1,6 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
 module.exports = class Usuario {
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(mi_username, mi_password) {
@@ -7,10 +9,14 @@ module.exports = class Usuario {
     }
     //Este método servirá para guardar de manera persistente el nuevo objeto. 
     save() {
-        return db.execute(
-            'INSERT INTO usuarios(username, password) VALUES (?, ?)', 
-            [this.username, this.password]
-        );
+        return bcrypt.hash(this.password, 12).then((password_cifrado) => {
+            return db.execute(
+                'INSERT INTO usuarios(username, password) VALUES (?, ?)', 
+                [this.username, password_cifrado]
+            );
+        }).catch((error) => {
+            console.log(error);
+        });
     }
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
